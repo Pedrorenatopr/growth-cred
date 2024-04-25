be67e42d919322908de84df59197afe5d7ac0601
pedrorenatopr <pedrorenato19@hotmail.com>
2024-04-25 12:42:36 -0300

    ajuste carrossel

--- carrossel-principal.js
+++ carrossel-principal.js
@@ -1,10 +1,10 @@
 function changeImage() {
     var image = document.getElementById('myImage');
-    if (image.src.endsWith('pf-teste.png')) {
+    if (image.src.endsWith('consignado-teste.png')) {
       image.src = 'pj-teste-3.png';
       image.alt = 'Imagem 2';
     } else {
-      image.src = 'pf-teste.png';
+      image.src = 'consignado-teste.png';
       image.alt = 'Imagem 1';
     }
   }
